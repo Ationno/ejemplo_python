6019edeffd293434307d7b5ebae9e093e9fc0863
Antonio Glorioso <antonioglorioso1@hotmail.com>
2022-03-16 21:45:14 -0300

    Arreglo de app

--- adivino.py
+++ adivino.py
@@ -1,8 +1,8 @@
 # Adivina adivinador....
 import random
-numero_aleatorio = random.randrange(101)
+numero_aleatorio = random.randrange(100)
 gane = False
-print("Tenés 3 intentos para adivinar un entre 0 y 99")
+print("Tenés 3 intentos para adivinar un entre 0 y 100")
 intento = 1
 while intento < 6 and not gane:
     numero_ingresado = int(input('Ingresa tu número: '))
